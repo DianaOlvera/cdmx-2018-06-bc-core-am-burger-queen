27b3a073d61c6311a1e2446a961544a960c41646
DianaOlvera <diana.saamaa@gmail.com>
2018-10-26 12:05:37 -0500

    agregando credenciales

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    // Initialize Firebase
+    apiKey: "AIzaSyDzUkvNCmsmAbqbI4xF1CeeLSpPiOLeHYg",
+    authDomain: "social-network-6eb8a.firebaseapp.com",
+    databaseURL: "https://social-network-6eb8a.firebaseio.com",
+    projectId: "social-network-6eb8a",
+    storageBucket: "social-network-6eb8a.appspot.com",
+    messagingSenderId: "250841734328"
+  } 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:{
+    // Initialize Firebase
+    apiKey: "AIzaSyDzUkvNCmsmAbqbI4xF1CeeLSpPiOLeHYg",
+    authDomain: "social-network-6eb8a.firebaseapp.com",
+    databaseURL: "https://social-network-6eb8a.firebaseio.com",
+    projectId: "social-network-6eb8a",
+    storageBucket: "social-network-6eb8a.appspot.com",
+    messagingSenderId: "250841734328"
+  } 
 };
 
 /*
